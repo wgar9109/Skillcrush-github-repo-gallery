0edd816076040504638fa8f133cd19ada9773cf0
wgar9109 <tylergary09@gmail.com>
2023-02-25 10:41:27 -0600

    Placed under version control

---
+++ js/script.js
@@ -0,0 +1,11 @@
+    var button = document.querySelector(".show-cat");
+    var button = document.querySelector(".cat");
+    
+    console.log(button);
+    console.log(cat);
+
+    button.addEventListener("click", function () {
+        cat.classList.add("show");
+    });
+
+
